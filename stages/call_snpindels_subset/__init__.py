985b1a960d80bedee7db563a428b1433adf66f66
Ian Fiddes <ian.fiddes@10xgenomics.com>
2018-04-12 18:25:09 -0700

    Name bug fixes.

--- stages/call_snpindels_subset/__init__.py
+++ stages/call_snpindels_subset/__init__.py
@@ -3,7 +3,6 @@ This stage is a wrapper for GATK
 """
 import martian
 import os
-import re
 import subprocess
 
 __MRO__ = '''
@@ -28,7 +27,7 @@ def split(args):
     """
     Perform split
     """
-    return {'chunks': [{'subset_bam':bam, 'node_id': n, '__mem_gb': 6} for bam, n in zip(args.subset_bams,
+    return {'chunks': [{'subset_bam':bam, 'node_id': n, '__mem_gb': 8} for bam, n in zip(args.subset_bams,
                                                                                          args.node_ids)]}
 
 
@@ -36,16 +35,27 @@ def main(args, outs):
     """
     Wrapper for GATK
     """
-    tmp = martian.make_path('{}.vcf'.format(args.node_id))  # node ID in original tree
+    tmp = martian.make_path('tmp.vcf')
     ref = os.path.join(args.reference_path, 'fasta', 'genome.fa')
     cmd = ['java', '-jar', args.gatk_path, 'HaplotypeCaller',
            '-R', ref, '-L', args.targets_file,
            '-I', args.subset_bam, '-O', tmp,
            '--native-pair-hmm-threads', '1']
     subprocess.check_call(cmd)
-    subprocess.check_call(['bgzip', tmp])
-    subprocess.check_call(['tabix', '-p', 'vcf', tmp + '.gz'])
-    outs.subset_variants = tmp + '.gz'
+
+    # fix the name
+    tmp2 = martian.make_path('{}.vcf'.format(args.node_id))  # node ID in original tree
+    with open(tmp2, 'w') as outf:
+        for l in open(tmp):
+            if l.startswith('#CHROM'):
+                l = l.split()
+                l[-1] = args.node_id
+                l = '\t'.join(l) + '\n'
+            outf.write(l)
+
+    subprocess.check_call(['bgzip', tmp2])
+    subprocess.check_call(['tabix', '-p', 'vcf', tmp2 + '.gz'])
+    outs.subset_variants = tmp2 + '.gz'
 
 
 def join(args, outs, chunk_defs, chunk_outs):
@@ -55,15 +65,8 @@ def join(args, outs, chunk_defs, chunk_outs):
     outs.coerce_strings()
     vcfs = [x.subset_variants for x in chunk_outs]
     cmd = ['vcf-merge'] + vcfs
-    #with open(outs.variants, 'w') as outf:
-    tmp = martian.make_path('tmp.vcf')
-    with open(tmp, 'w') as outf:
-        subprocess.check_call(cmd, stdout=outf)
-    # fix the header
-    r = re.compile('chnk[0-9A-Za-z-]+\/files\/([0-9]+)_[0-9]+')
     with open(outs.variants, 'w') as outf:
-        for l in open(tmp):
-            outf.write(r.sub('\\1', l))
+        subprocess.check_call(cmd, stdout=outf)
     with open(outs.barcode_map, 'w') as outf:
         for node_id, bcodes in sorted(zip(args.node_ids, args.barcode_subsets), key=lambda x: int(x[0])):
             outf.write('\t'.join(map(str, [node_id, bcodes])) + '\n')
